cdc3d28fb6c92a2d5fa824386ae2ca1308cdadca
Samuel <sam.makes.programs@gmail.com>
2025-09-10 13:36:22 -0300

    working(not completed) forms

--- src/components/apps/FormsApp/FormsApp.js
+++ src/components/apps/FormsApp/FormsApp.js
@@ -1,7 +1,25 @@
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().min(4).max(20).required(),
+    confirmpassword: yup
+      .string()
+      .oneOf([yup.ref("password")], "Passwords must match")
+      .required(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data) => {
     console.log(data);
@@ -9,6 +27,7 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="Full Name..." {...register("fullName")} />
+      <p>{errors.fullName?.message}</p>
       <input type="text" placeholder="Email..." {...register("email")} />
       <input
         type="password"
